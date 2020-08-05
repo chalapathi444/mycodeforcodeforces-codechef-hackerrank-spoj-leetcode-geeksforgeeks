149ac01b1bd7ea74f8157fab1dd94b195ee28fb7
bhupathiraju chalapathi naga varma <48557095+chalapathi444@users.noreply.github.com>
2020-08-05 17:39:55 +0530

    solution for codeforces problem 798A

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,25 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+int is_palindrome(string &a,const int &len)
+{
+	int half = len/2;
+	int count = 0;
+	for(int i = 0; i < half; i++)
+	{
+		if(a[i] != a[len-i-1])
+		{
+			count++;
+		}
+	}
+	return count;
+}
 int main()
 {
-	signed a;
+	string a;
 	cin>>a;
-	signed first = a -1 ;
-	signed second = 1;
-	for(signed i = 0; i < a/2; i++)
+	char temp;
+	int len = a.length();
+	int count = is_palindrome(a,len);
+	if(count == 1 && len%2 == 0)
 	{
-		if(first <= 0 || second <= 0)
-		{
-			cout<<"NO";
-			return 0;
-		}
-		if(first%2 == 0 && second%2 == 0)
-		{
-			cout<<"YES";
-			return 0;
-		}
-		first--;
-		second++;
+		cout<<"YES";
+		return 0;
+	}
+	if(count <= 1 && len%2 != 0)
+	{
+		cout<<"YES"<<endl;
+		return 0;
 	}
 	cout<<"NO";
 	return 0;
